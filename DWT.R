3eda24906b8288f9be213712b9e0fd4d9deb51fc
曹 鹤洋 <heyang@Heyangs-MacBook-Pro.local>
2018-07-01 22:13:31 -0400

    Final

--- DWT.R
+++ DWT.R
@@ -2,14 +2,63 @@ index = 141
 Ret = readRDS("Ret.RDS")
 
 nObs = nrow(Ret)
-# nObs = 2620
+nObs = 2^12
 RetData = Ret[,-1]
 nVar = ncol(RetData)-1
 Z = unlist(RetData[index])[1:nObs]
-# library(xts)
-# Z = xts(Z,order.by = Ret$date)
 
 library(wavelets)
-wt = dwt(Z)
-wt = align(wt)
-p = plot(wt)
+library(ggplot2)
+library(gridExtra)
+library(reshape2)
+days = Ret$date[1:nObs]
+#### wavelet transform
+wt =  dwt(Z, n.levels=12,boundary="reflection", fast = FALSE)
+Z_rec = data.frame(date = days)
+
+#### reconstruction
+p <- list()
+for (i in 1:12) {
+  # i = 2
+  
+  tmp = idwt(wt)
+  Z_rec = cbind.data.frame(Z_rec,tmp)
+  colnames(Z_rec)[i+1] = paste("resolution at",i)
+  tmp = Z_rec[,c(1,i+1)]
+  colnames(tmp) = c("date","signal")
+  p[[i]] = ggplot(data = tmp)+geom_line(aes(x = date, y = signal))+ylab(paste("scale at",i))
+  wt@W[[i]][] = 0
+}
+grid.arrange(grobs = p)
+
+#### this part is for visualize each component
+wt =  dwt(Z, n.levels=12,boundary="reflection", fast = FALSE)
+wt_origin = wt
+wtcoef = list()
+for (i in 1:12) {
+  wtcoef[[i]] = wt@W[[i]]
+  wt@W[[i]][] = 0
+}
+
+
+decomposed = data.frame(date = days)
+p1 <- list()
+for (i in 1:12) {
+  # i = 1
+  wtmp = wt
+  # wtmp
+  wtmp@W[[i]] = wtcoef[[i]]
+  decomposed = cbind.data.frame(decomposed,idwt(wtmp))
+  colnames(decomposed)[i+1] = paste("component at level",i)
+  tmp = decomposed[,c(1,i+1)]
+  colnames(tmp) = c("date","signal")
+  p1[[i]] = ggplot(data = tmp) +
+    geom_line(aes(x = date, y = signal)) +
+    ylab(paste("scale at",i)) +
+    theme(axis.title.x=element_blank())
+}
+grid.arrange(grobs = p1)
+decomposed_melted = melt(decomposed,id = "date")
+ggplot(data = decomposed_melted,aes(x = date,y=value,colour = variable)) +
+  geom_line(alpha = 0.4) +
+  theme_bw()
--- PCAICA.R
+++ PCAICA.R
@@ -7,45 +7,37 @@ RetData = Ret[,-1]
 nVar = ncol(RetData)-1
 inSample = round(nObs/2)
 outSample = nObs - inSample
-# hc = hclust(dist(t(RetData)))
-corr = rep(NA,ncol(RetData))
-for (i in 1:ncol(RetData)) {
-  df = merge(x = Ret[1:inSample,c(1,index+1)], y = Ret[1:inSample,c(1,i+1)], by = "date")
-  df = df[complete.cases(df),]
-  scaler = nrow(df)/inSample
-  if (scaler>0) {
-    corr[i] = cor(df[,2],df[,3])*scaler
-  }
-}
-# picked = order(-corr)[2:11]
+
 r = RetData[1:inSample,]
 r = r[ , colSums(is.na(r)) == 0]
+
+#### PCA
 r = as.matrix(r)
 m = colMeans(r)
 r = sweep(r,2,m)
 c = cov(r)
 e = eigen(c)
-# RetData[is.na(RetData)] = 0
-# e0 = eigen(cov(RetData))
+#### loadings
 loadings = sweep(e$vectors,2,sqrt(e$values),"*")
+#### principle components
 PCs = r%*%loadings
 w = solve(loadings)
-# PCs = sweep(PCs,2,sqrt(e$values),"*")
-# w = solve(sweep(e$vectors,2,sqrt(e$values),'*'))
+#### pcs' cumulative returns
 PCs_tr = apply(PCs,2, function(x) cumprod(1+x))
 PCs_tr = as.data.frame(PCs_tr)
 PCs_tr = cbind.data.frame(Ret$date[1:inSample],PCs_tr)
 colnames(PCs_tr)[1] = "date"
 
-
-
 # pca = prcomp(r)
+#### PCs' explanation power
 ExpPwr = e$values/sum(e$values)
 cumExpPwr = cumsum(ExpPwr)
+
 PCA_stat = data.frame(PCs = 1:length(e$values),
                       "Marginal Explaining Power" = ExpPwr, 
                       "Cumulative Explaining Power" = cumExpPwr)
 
+#### Visualization
 library(reshape2)
 PCA_stat_scaled = PCA_stat
 PCA_stat_scaled$Marginal.Explaining.Power = PCA_stat_scaled$Marginal.Explaining.Power*4
@@ -86,27 +78,60 @@ ggplot(PCA_pred, aes(x=error.at.t)) +
   geom_density(alpha=.2, fill="#FF6666") +
   geom_vline(aes(xintercept=mean(error.at.t)), color="blue", linetype="dashed", size=1)
 
+####ICA
+
 library(fastICA)
-ica = fastICA(r,371)
-r_est_ica = ica$S[,1:5]%*%ica$A[1:5,]
+
+#### Calculate correlations
+corr = rep(NA,ncol(RetData))
+
+for (i in 1:ncol(RetData)) {
+  df = merge(x = Ret[1:inSample,c(1,index+1)], y = Ret[1:inSample,c(1,i+1)], by = "date")
+  df = df[complete.cases(df),]
+  scaler = nrow(df)/inSample
+  if (scaler>0) {
+    corr[i] = cor(df[,2],df[,3])*scaler
+  }
+}
+
+picked = order(corr)[2]
+Sp500ret = cbind.data.frame(date = Ret$date, gain = rowMeans(Ret[,-1],na.rm = TRUE))
+
+#### find market beta
+reg_1 = lm(r[,index]~Sp500ret$gain[1:inSample])
+reg_2 = lm(r[,picked]~Sp500ret$gain[1:inSample])
+#### use residues for analysis
+ica = fastICA(cbind(reg_1$residuals,reg_2$residuals),2)
+#### scale manually
+S = ica$S/30
+A = ica$A*30
+r_est_ica = S%*%A
+STr = apply(S, 2, function(x) cumprod(1+x))
+Signal = data.frame(date = Ret$date[1:inSample], Signal_1 = STr[,1], Signal_2 = STr[,2])
+Signal_melted = melt(Signal, id = "date")
+ggplot(data = Signal_melted,
+       aes(x = date, y = value, colour = variable)) +
+  geom_line()+theme_bw()
+
+
 # e_ica = abs(r - r_est_ica)
-e_ica = abs(r[inSample,] - r_est_ica[inSample,])
-ICA_pred = data.frame(stock = 1:ncol(r),
-                      "return at t" = r[inSample,], 
-                      "estimate at t" = r_est_ica[inSample,], 
-                      "error at t" = e_ica)
-
-ICA_pred_melted = melt(ICA_pred[,1:3],id = "stock")
-
-ggplot(data = ICA_pred_melted,
-       aes(x = stock, y = value, colour = variable)) +
-  geom_line()
-
-ggplot(data=ICA_pred_melted, aes(x=stock, y=value, fill=variable)) +
-  geom_bar(stat="identity", position=position_dodge()) + theme_bw()
-
-ggplot(ICA_pred, aes(x=error.at.t)) + 
-  geom_histogram(color="black", fill="white",bins=40) + theme_bw()+
-  geom_density(alpha=.2, fill="#FF6666") +
-  geom_vline(aes(xintercept=mean(error.at.t)), color="blue", linetype="dashed", size=1)
+# e_ica = abs(r[inSample,] - r_est_ica[inSample,])
+# ICA_pred = data.frame(stock = 1:ncol(r),
+#                       "return at t" = r[inSample,], 
+#                       "estimate at t" = r_est_ica[inSample,], 
+#                       "error at t" = e_ica)
+# 
+# ICA_pred_melted = melt(ICA_pred[,1:3],id = "stock")
+# 
+# ggplot(data = ICA_pred_melted,
+#        aes(x = stock, y = value, colour = variable)) +
+#   geom_line()
+# 
+# ggplot(data=ICA_pred_melted, aes(x=stock, y=value, fill=variable)) +
+#   geom_bar(stat="identity", position=position_dodge()) + theme_bw()
+# 
+# ggplot(ICA_pred, aes(x=error.at.t)) + 
+#   geom_histogram(color="black", fill="white",bins=40) + theme_bw()+
+#   geom_density(alpha=.2, fill="#FF6666") +
+#   geom_vline(aes(xintercept=mean(error.at.t)), color="blue", linetype="dashed", size=1)
 
